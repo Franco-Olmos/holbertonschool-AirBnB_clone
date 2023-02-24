f0bafca31f08384c9a95cce55504e2763faa968b
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-02-24 12:13:07 -0800

    Update

--- models/base_model.py
+++ models/base_model.py
@@ -9,14 +9,19 @@ import datetime
 
 class BaseModel:
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            self.id = kwars["id"]
+            self.created_at = kwars["created_at"]
+            self.updated_at = kwars["updated_at"]
+        
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
 
     def __str__(self):
-        """????????????????"""
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
