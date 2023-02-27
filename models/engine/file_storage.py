c90991a75699aef59be7fac04febb8eb55414ee2
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-02-27 14:56:06 -0800

    Task 05

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,7 @@
 """FileStorage"""
 import json
 from os import path
+import os.path
 
 
 class FileStorage:
@@ -21,10 +22,13 @@ class FileStorage:
         for key, value in self.__objects.items():
             json_dict[key] = value.to_dict()
         with open(self.__file_path, "w", encoding='utf-8') as f:
-            f.write(json.dumps(json_dict))
+            json.dump(json_dict, f)
+            """f.write(json.dumps(json_dict))"""
 
     def reload(self):
-        if path.exists(self.__file_path):
-            with open(self.__file_path, encoding="utf-8") as f:
-                json_dict = f.read()
-                json.loads(json_dict)
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, "r", encoding="utf-8") as f:
+                json_dict = json.load(f)
+                """for key, value in json_dict.items():
+                    FileStorage.__objects[key] = BaseModel(**value)"""
+                """json_dict = json.loads(f.read())"""
