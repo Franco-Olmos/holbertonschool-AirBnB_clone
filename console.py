9c44925f0eaa106c18e6ca23a0e2349786a9a0f0
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-02-28 11:25:58 -0800

    Update

--- console.py
+++ console.py
@@ -4,16 +4,19 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
     file = None
 
     def do_quit(self, arg):
+        """ddddddd!"""
         return True
 
     def do_EOF(self, arg):
+        """ddddd"""
         return True
 
-    def do_emptyline(self, arg):
+    def do_emptyline(self):
+        """ddddddd"""
         pass
 
 if __name__ == '__main__':
