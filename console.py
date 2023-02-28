9fa3e7a6bb27cddd5280ce2327a44d5b99207dc2
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-02-28 11:14:16 -0800

    Task 06

--- console.py
+++ console.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
 """ shebang """
-import 
+import cmd
 
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb)"
+    file = None
+
+    def do_quit(self, arg):
+        return True
+
+    def do_EOF(self, arg):
+        return True
+
+    def do_emptyline(self, arg):
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
