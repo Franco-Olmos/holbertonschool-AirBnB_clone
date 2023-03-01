e644c133924c93bf15e618e5f3ed731fee592d19
Franco-Olmos <5749@holbertonstudents.com>
2023-02-28 18:14:18 -0800

    task9

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" shebang """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ Amenity Class """
+    name = ""
---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" shebang """
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ CityClass """
+    state_id = ""
+    name = ""
