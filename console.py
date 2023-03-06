a61998ca3bbeacb0b6b0fd7a9c6df802fadf2024
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-03-06 12:13:49 -0800

    Update

--- console.py
+++ console.py
@@ -15,8 +15,8 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
     file = None
     classes = {'BaseModel': BaseModel, 'User': User, 'State': State,
-            'City': City, 'Amenity': Amenity, 'Place': Place,
-            'Review': Review}
+               'City': City, 'Amenity': Amenity, 'Place': Place,
+               'Review': Review}
 
     def do_quit(self, arg):
         """dddddddddddddddddddddddddddddd"""
@@ -30,23 +30,51 @@ class HBNBCommand(cmd.Cmd):
         """dddddddddddddddddddddddddddddd"""
         pass
 
-    def do_create(self, argv):
+    """def do_create(self, argv):
         imput = args.split()
         if not imput:
             print("** class name missing **")
         if argv is not BaseModel:
+            print("** class doesn't exist **")"""
+    def do_create(self, argv):
+        """Create a new class and asigns it an id."""
+        inputs = argv.split()
+        if not inputs:
+            print("** class name missing **")
+        elif inputs[0] not in HBNBCommand.classes.keys():
             print("** class doesn't exist **")
+        else:
+            instance = HBNBCommand.classes[inputs[0]]()
+            print(instance.id)
+            instance.save()
+    
+    """def do_show(self, argv):
+        imput = argv.split()
+        if not imput:
+            print("** class name missing **")
+        if argv is not BaseModel:
+            print("** class doesn't exist **")
+        if len(argv) < 2:
+            print("** instance id missing **")"""
 
     def do_show(self, argv):
-        imput = args.split()
-        if not imput:
+        """Show all contents based on id or all if dont specified."""
+        inputs = argv.split()
+        if not inputs:
             print("** class name missing **")
-        if argv is not BaseModel:
+        elif inputs[0] not in HBNBCommand.classes.keys():
             print("** class doesn't exist **")
-        if len(argv) == 1:
+        elif len(inputs) < 2:
             print("** instance id missing **")
-        if __name__ == '__main__':
-            HBNBCommand().cmdloop()
+        else:
+            key = inputs[0] + '.' + inputs[1]
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                all_objs = storage.all()
+                key = inputs[0] + '.' + inputs[1]
+                if key in all_objs.keys():
+                    print(all_objs[key])
 
     def do_destroy(self, arg):
         if len(arg) == 0:
@@ -67,7 +95,7 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** instance id missing **")
 
-    def do_all(self, arg):
+    """def do_all(self, arg):
         if len(arg) == 0:
             print([str(a) for a in storage.all().values()])
             return
@@ -76,7 +104,22 @@ class HBNBCommand(cmd.Cmd):
             return
         if arg in self.classes.keys():
             for a in storage.all():
-                print(str(a))
+                print(str(a))"""
+
+    def do_all(self, argv):
+        """Show all contents based on id or all if dont specified."""
+        inputs = argv.split()
+        if not inputs:
+            print(list((storage.all())))
+        elif inputs[0] not in HBNBCommand.classes.keys():
+            print("** class doesn't exist **")
+        else:
+            list_all = []
+            storage.reload()
+            for obj_id in storage.all().keys():
+                if obj_id.split(".")[0] == inputs[0]:
+                    list_all.append(str(storage.all()[obj_id]))
+            print(list_all)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
